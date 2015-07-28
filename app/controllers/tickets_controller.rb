a4149a60aefdc1547c4004b1201590a8491291c4
omfaer <omfaer@bil.omu.edu.tr>
2015-07-28 16:04:10 +0300

    ticket controller ve view sayfaları düzeltildi

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,37 +1,47 @@
 class TicketsController < ApplicationController
   def index
-  	@ticket = Ticket.all
-  end
-
-  def create
-  	@ticket = Ticket.new(ticket_params)
-
-  	if @ticket.save
-  		redirect_to @article
-  	else
-  		render 'new'
-  	end
+    @tickets = Ticket.all
   end
 
   def new
-  	@ticket = Ticket.new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+
+    if @ticket.save
+      redirect_to @ticket
+    else
+      render 'new'
+    end
   end
 
   def show
-  	@ticket = Ticket.find(params[:id])
+    @ticket = Ticket.find(params[:id])
   end
 
   def edit
-  	@ticket = Ticket.find(params[:id])
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+
+    if @ticket.update(ticket_params)
+      redirect_to @ticket
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-  	@ticket = Ticket.find(params[:id])
-  	@ticket.destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
   end
 
   private
-  	def ticket_params
-  		params.require(:article).permit(:title, :text)
-  	end
+    def ticket_params
+      params.require(:ticket).permit(:title, :category, :content)
+    end
 end
